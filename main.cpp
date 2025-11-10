c55f8b5c89b4f6fda3b5fbc9074e6c4cbb0791cf
HSU,WEI-YU <41343121@nfu.edu.tw>
2025-11-10 16:28:49 +0800

    2

--- main.cpp
+++ main.cpp
@@ -2,10 +2,31 @@
 
 #include <QApplication>
 
+#include <QApplication>
+#include <QMainWindow>
+#include <QScreen>
+
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    Window w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    QMainWindow *w1 = new QMainWindow;
+    w1->setWindowTitle("Window 1");
+    w1->resize(480, 320);
+
+    QMainWindow *w2 = new QMainWindow;
+    w2->setWindowTitle("Window 2");
+    w2->resize(480, 320);
+
+    w1->show();
+
+
+    int gap = 10;
+    QRect geom1 = w1->frameGeometry();
+    QPoint posForW2(geom1.topRight().x() + gap, geom1.topRight().y());
+    w2->move(posForW2);
+    w2->show();
+
+
+    return app.exec();
 }
